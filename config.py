2947ff5be674eddd6d16841656774a980c7a66fd
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2025-03-05 19:27:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "29861481"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "33abbe2e6c767868409d18a099fae999"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "7710642242"
+SUPPORT_GRP = "knight_society"
+UPDATE_CHNL = "itsRareBeautySelenaGomez"
+OWNER_USERNAME = "smartness_to_hai"
 
